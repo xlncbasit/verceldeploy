5101327034057b31deee85eb3a12e5c8174aad59
xlncbasit <82083684+xlncbasit@users.noreply.github.com>
2024-11-10 00:40:34 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { Suspense } from 'react';
 import RedirectComponent from '@/components/RedirectComponent';
+import Loading from '@/components/ui/loading';
 
 export default function Home() {
-  return <RedirectComponent />;
+  return (
+    <Suspense fallback={<Loading />}>
+      <RedirectComponent />
+    </Suspense>
+  );
 }
